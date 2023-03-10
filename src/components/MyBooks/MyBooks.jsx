ac97b516ee358491752e641d43933c49b4032a7f
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-03-10 21:20:05 +0100

    aadd

--- src/components/MyBooks/MyBooks.jsx
+++ src/components/MyBooks/MyBooks.jsx
@@ -7,7 +7,7 @@ import styles from './my-books.module.scss';
 const MyBooks = () => {
   return (
     <div>
-      <h3>Phonebook</h3>
+      <h3>Phonebook_kk</h3>
       <div className={styles.wrapper}>
         <div className={styles.block}>
           <h4>Add new contact</h4>
